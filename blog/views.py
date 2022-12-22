f3799849a26fb025bcaa6a489d0031e02bba710b
OxanaSF <oxanatango@gmail.com>
2022-12-22 12:20:34 -0800

    convert views to class based view

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,55 @@
-from django.shortcuts import render, get_object_or_404
+
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
 
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+# def starting_page(request):
+#     latest_posts = Post.objects.all().order_by("-date")[:3]
+
+#     return render(request, "blog/index.html", {
+#         "posts": latest_posts
+#     })
 
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+    # def posts(request):
+    #     all_posts = Post.objects.all().order_by("-date")
+    #     return render(request, "blog/all-posts.html", {
+    #         "all_posts": all_posts
+    #     })
 
 
-def post_detail(request, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
 
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "tags": identified_post.tags.all()
-    })
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] = self.object.tags.all()
+        return context
+
+
+# def post_detail(request, slug):
+#     identified_post = get_object_or_404(Post, slug=slug)
+
+#     return render(request, "blog/post-detail.html", {
+#         "post": identified_post,
+#         "tags": identified_post.tags.all()
+#     })
